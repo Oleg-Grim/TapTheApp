e95843cfbe96818a50c268dd0a1d6b3a047a3dde
Oleg-Grim <74546107+Oleg-Grim@users.noreply.github.com>
2021-02-26 23:53:23 +0300

    Complete code for the game

---
+++ MainMenuTweens.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class MainMenuTweens : MonoBehaviour
+{
+    public GameObject playButton;
+    public GameObject shopButton;
+
+    public GameObject tap;
+    public Vector2 tapPos;
+    public GameObject the;
+    public Vector2 thePos;
+    public GameObject app;
+    public Vector2 appPos;
+    public GameObject hiscoreText;
+
+    private void Awake()
+    {
+        StartCoroutine("TweenIn");
+    }
+
+    private IEnumerator TweenIn()
+    {
+        yield return new WaitForSecondsRealtime(0.1f);
+
+        LeanTween.moveLocal(app, appPos, 0.2f);
+        yield return new WaitForSecondsRealtime(0.2f);
+        LeanTween.moveLocal(tap, tapPos, 0.2f);
+        yield return new WaitForSecondsRealtime(0.3f);
+        LeanTween.moveLocal(the, thePos, 0.3f);
+        yield return new WaitForSecondsRealtime(0.3f);
+        LeanTween.moveLocalX(playButton, 0f, 0.3f);
+        yield return new WaitForSecondsRealtime(0.1f);
+        LeanTween.moveLocalX(shopButton, 0, 0.3f);
+        LeanTween.scaleX(hiscoreText, 1, 0.3f);
+
+        StopCoroutine("TweenIn");
+    }
+
+    public void PressStart()
+    {
+        StartCoroutine("TweenOut");
+    }
+
+    private IEnumerator TweenOut()
+    {
+        LeanTween.moveLocalX(app, 3000f, 0.25f);
+        LeanTween.moveLocalY(tap, 2000, 0.25f);
+        LeanTween.moveLocalX(the, -3000, 0.25f);
+        LeanTween.moveLocalX(playButton, -3000f, 0.25f);
+        LeanTween.moveLocalX(shopButton, 3000, 0.25f);
+        LeanTween.scaleX(hiscoreText, 0, 0.25f);
+        yield return new WaitForSecondsRealtime(0.25f);
+        ControllerScript.instance.ReloadTheGame();
+        StopCoroutine("TweenOut");
+    }
+
+}
---
+++ UI_Controller.cs
@@ -0,0 +1,100 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+public class UI_Controller : MonoBehaviour
+{
+    public Text levelScoreText;
+    public Text currencyText;
+
+    public GameObject buyButton;
+
+    private GameObject[] UI_Elements;
+    public GameObject activeUI;
+
+    public TextMeshProUGUI recordScore, recordLevel;
+    public int toPass;
+
+    private void Awake()
+    {
+        UI_Elements = new GameObject[transform.childCount];
+        for (int i = 0; i < UI_Elements.Length; i++)
+        {
+            UI_Elements[i] = transform.GetChild(i).gameObject;
+        }
+
+        activeUI = UI_Elements[1];
+        
+        BossCheck(ControllerScript.bossCounter);
+
+        activeUI.SetActive(true);
+    }
+
+    public void SwapMenus(int index)
+    {
+        activeUI.SetActive(false);
+        activeUI = UI_Elements[index];
+        if (activeUI.name == "(0)Main_Menu_Holder")
+        {
+            ControllerScript.instance.theTarget.SetActive(false);
+        }
+
+        activeUI.SetActive(true);
+    }
+
+    public Text endgameScoreText;
+    public Text hiStageText;
+
+    public void EndScore(int score, int stage)
+    {
+        endgameScoreText.text = score.ToString();
+
+        hiStageText.text = stage.ToString();
+    }
+
+    public void RecordScores(int score, int stage)
+    {
+        recordLevel.text = "STAGE: " + stage.ToString();
+        recordScore.text = "SCORE: " + score.ToString();
+    }
+
+    public Text buyButtonText;
+
+    public void TweenButton(bool show, int cost)
+    {
+
+        buyButtonText.text = "BUY FOR " + cost.ToString();
+
+        if (show)
+            LeanTween.scale(buyButton, Vector3.one, 0.1f);
+        else
+            LeanTween.scale(buyButton, Vector3.zero, 0.1f);
+    }
+
+    public void ShakeButton()
+    {
+        LeanTween.moveLocalX(buyButton, buyButton.transform.position.x, 0.15f).setEaseShake();
+    }
+
+    public Image cursorIcon;
+
+    public void UpdateCursorIcon(Sprite sprite)
+    {
+        cursorIcon.sprite = sprite;
+    }
+
+    public GameObject[] bossSprites;
+
+    public void BossCheck(int count)
+    {
+        for (int i = 0; i < count; i++)
+        {
+            bossSprites[i].GetComponent<ShopItem>().referenceSO.isObtained = true;
+        }
+    }
+}
+
+
+    
